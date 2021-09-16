37d0f531238c0ef05a4c3e098affe2ac3b755394
Björn Stresing <bjoern.stresing@gmx.de>
2021-09-16 21:00:45 +0200

    Fix windows build

--- conanfile.py
+++ conanfile.py
@@ -26,22 +26,16 @@ class QtWebAppConan(ConanFile):
         tools.get("http://stefanfrings.de/qtwebapp/QtWebApp.zip")
         tools.replace_in_file("QtWebApp/QtWebApp/QtWebApp.pro", "CONFIG(debug, debug|release)", "CONFIG(ignore, debug|release|ignore)")
 
-    def configure(self):
-        if self.settings.os == 'Android':
-            self.options["Qt"].qtandroidextras = True
-
     def build(self):
 
-        build_type = "debug" if self.settings.build_type == "Debug" else "release"
-
         with tools.environment_append({"PATH": self.deps_cpp_info["Qt"].bin_paths}):
-            if self.settings.os == "Windows":
+            if self.settings.compiler == "Visual Studio":
                 env_build = VisualStudioBuildEnvironment(self)
                 with tools.environment_append(env_build.vars):
                     vcvars = tools.vcvars_command(self.settings)
                     self.run('%s && qmake -r -tp vc QtWebApp.pro' % (vcvars), cwd="QtWebApp/QtWebApp")
                     ms_env = MSBuild(self)
-                    ms_env.build(project_file="QtWebApp/QtWebApp.sln", upgrade_project=False)
+                    ms_env.build(project_file="QtWebApp/QtWebApp/QtWebApp.vcxproj", upgrade_project=False)
             else:
                 autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
                 envvars = autotools.vars
@@ -54,11 +48,11 @@ class QtWebAppConan(ConanFile):
 
     def package(self):
 
+        build_type = "debug" if self.settings.build_type == "Debug" else "release"
         arch = "*"
-        if self.settings.os == "Windows":
-            self.copy("QtAV/lib_win_" + arch + "/*Qt*AV*.lib", dst="lib", keep_path=False)
-        elif self.settings.os == "Android":
-            self.copy("QtAV/lib_android_*" + "/*Qt*AV*.so", dst="lib", keep_path=False)
+        if self.settings.compiler == "Visual Studio":
+            self.copy("QtWebApp/QtWebApp/" + build_type + "*.lib", dst="lib", keep_path=False)
+            self.copy("QtWebApp/QtWebApp/" + build_type + "*.dll", dst="lib", keep_path=False)
         else:
             self.copy("QtWebApp/QtWebApp/*.so*", dst="lib", keep_path=False, symlinks=True)
         self.copy("QtWebApp/QtWebApp/httpserver/*.h", dst="include/httpserver", keep_path=False)
